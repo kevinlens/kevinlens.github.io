537356514cb1cd9a9366fec32c7c4d1fc5379002
Kevin <kevinlenske@proton.me>
2023-03-24 00:37:19 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  //look for any html files and compile that into a static style sheet 
+  content: ['./*.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+ 
