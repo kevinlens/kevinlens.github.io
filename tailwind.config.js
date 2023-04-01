f3577518a60ee0e4e90818411321cab3d6d19fe3
Kevin <kevinlenske@proton.me>
2023-03-31 23:29:42 -0700

    added a bunch of stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,30 @@ module.exports = {
   //look for any html files and compile that into a static style sheet 
   content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        '1xl': { max: '1400px' },
+        // => @media (max-width: 1535px) { ... }
+        '-1xl': { min: '1400px' },
+        // => @media (min-width: 1535px) { ... }
+        '-lxl': { min: '1300px' },
+        // => @media (min-width: 1535px) { ... }
+        'lxl': { max: '1300px' },
+        // => @media (min-width: 1535px) { ... }
+        '1lg': { max: '1100px' },
+        // => @media (min-width: 1535px) { ... }
+        '-1lg': { min: '1100px' },
+        // => @media (min-width: 1535px) { ... }
+        '-lg': { max: '1000px' },
+        // => @media (min-width: 1535px) { ... }
+        '-xmd': { max: '900px' },
+        // => @media (min-width: 1535px) { ... }
+        'xmd': { min: '900px' },
+        // => @media (min-width: 1535px) { ... }
+        '-sm': { max: '540px' },
+        // => @media (min-width: 1535px) { ... }
+      }
+    },
   },
   plugins: [],
 }
